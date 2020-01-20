89fe29ca57438ba12007ce4fd422d794e4197e1e
Whitefallen <thomas.kammann@study.hs-duesseldorf.de>
2020-01-20 14:16:19 +0100

    Refined

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  showFiller = false;
 
   constructor() { }
 
