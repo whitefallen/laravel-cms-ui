16ac607dbe77a43cceb7dd2f8d852b608f7c6092
Jonas Baur <jonasbaur@hotmail.de>
2020-01-24 13:41:22 +0100

    added functionality to validate add format form and submit the form to create a new format in backend db

--- src/app/addformat/addformat.component.ts
+++ src/app/addformat/addformat.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {RequestService} from '../requestService';
+import {Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-addformat',
   templateUrl: './addformat.component.html',
-  styleUrls: ['./addformat.component.css']
+  styleUrls: ['./addformat.component.css'],
+  providers: [RequestService]
 })
 export class AddformatComponent implements OnInit {
+  name: string;
+  description: string;
 
-  constructor() { }
+  constructor(private requestService: RequestService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  add(addForm: NgForm) {
+    this.requestService.addFormat(
+      addForm.value.formatName, addForm.value.formatDescription, sessionStorage.getItem('userid'), sessionStorage.getItem('userid')
+    ).subscribe((data: any) => {
+      if(data.info === 1) {
+        this.router.navigate(['/dashboard/format']);
+      }else{
+        alert('something went wrong');
+      }
+    });
+
   }
 
 }
